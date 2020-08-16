b945c475a5b6dfd1cc4f5c979897a3bc3ba34388
Juan Ramos <juancagb.17@gmail.com>
2020-08-15 22:57:48 -0500

    Add reset endpoint

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class ResetController extends Controller
 {
-    //
+    public function reset()
+    {
+        // Reset state before starting tests
+        
+        Artisan::call('migrate:fresh');
+        
+        return 'OK'; // status code is 200
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Reset state before starting tests
-// POST /reset
-// 200 OK
-
 Route::post('/reset', 'ResetController@reset');
 
 Route::get('/balance', 'BalanceController@show');
